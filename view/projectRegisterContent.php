464f13e39ec2b5593c9c8dd3de7e30605a56fdf4
Matoro17 <gabrielsilvadeazevedo@gmail.com>
2018-01-27 01:42:36 -0200

    fix

--- view/projectRegisterContent.php
+++ view/projectRegisterContent.php
@@ -40,15 +40,10 @@
         <div class="form-group row">
           <label for="text-input" class="col-lg-2 col-form-label">Desenvolvedores</label>
           <div class="col-lg-5" id="divdodev">
-<<<<<<< HEAD
 
             <input type="hidden" name="quant" value="1" id="javu">
 
             <select name="developers0">
-          
-=======
-            <select>         
->>>>>>> d56331fe8d4310987a942f72b05f9d1085fe48c9
                 <?php
                           $size = $_SESSION['size']['MAX(id)'];
                           $control = new MembersController();
@@ -62,12 +57,9 @@
             </select>
             <button type="button" class="btn" id="btndev">+1 Dev</button>
           </div>
-<<<<<<< HEAD
+
         </div>
-                          
-=======
-        </div>                
->>>>>>> d56331fe8d4310987a942f72b05f9d1085fe48c9
+
         <div style="margin-bottom: 20px;">
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input class="btn" type="submit" name="registerProjectAttempt" value="Enviar" align="center">
         </div>            
